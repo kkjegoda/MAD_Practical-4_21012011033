26044e6bff768d0c8f9bfa0a6132ba914eba505a
kj <kkjegoda@gmail.com>
2023-08-07 20:29:20 +0530

    Practical-4

---
+++ app/src/main/java/com/example/mad_practical_4_21012011033/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mad_practical_4_21012011033
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
